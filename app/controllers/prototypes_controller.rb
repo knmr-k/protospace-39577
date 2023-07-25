1780bdbf066147b2853a8f3414c867c927852f6a
knmr-k <kanamr.kz@gmail.com>
2023-07-25 16:31:38 +0900

    authenticate_user!の設定値の修正
    ログアウト状態のユーザーは、プロトタイプ新規投稿ページ・プロトタイプ編集ページに遷移しようとすると、ログインページにリダイレクトされること

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :index, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
     @prototype = Prototype.all
